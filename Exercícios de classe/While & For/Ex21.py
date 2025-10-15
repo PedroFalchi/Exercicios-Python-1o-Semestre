9ccf244d8f34d931de70ed1867de61443c2b0390
Laboratório FIAP <rm566967@fiap.com.br>
2025-10-14 22:10:07 -0300

    segundo commit

--- Exercícios de classe/While & For/Ex21.py
+++ Exercícios de classe/While & For/Ex21.py
@@ -4,11 +4,31 @@ while(i <= 10):
     print(i)
     i = i + 1 '''
 
-num = int(input('Digite um número para obter a tabuada: '))
+x = int(input('Digite um número para obter a tabuada: '))
 
-while(num <= 0):
-    print("Não pode número negativo!")
-    num = int(input('Digite um outro numero: '))
 
-for i in range(1, 11, 1):
-    print(i)
+while (x <= 0):
+    print('Erro! Digite apenas números positivos!')
+    x = int(input('Digite um número para obter a tabuada: '))
+
+
+a = int(input('Digite o intervalo inicial: '))
+b = int(input('Digite o intervalo final: '))
+
+
+while ( (a <= 0) or (b <= 0) ):
+    print('Erro! O intervalo inicial e final devem ser positivos!')
+    a = int(input('Digite o intervalo inicial: '))
+    b = int(input('Digite o intervalo final: '))
+
+
+while (b <= a):
+    print('Erro! O intervalo final deve ser maior do que o inicial!')
+    b = int(input('Digite o intervalo final: '))
+
+
+while(a <= b):
+    r = x * b
+    print(f'{x} X {b} = {r}')
+    b = b - 1
+print('FIM')
